75ab8de417aee67231b5a35ac3c8742f2ed8c679
Carl Masak <cmasak@gmail.com>
2009-10-10 19:07:37 +0200

    [GGE::OPTable] now handles circumfix: operators

--- lib/GGE/OPTable.pm
+++ lib/GGE/OPTable.pm
@@ -5,18 +5,30 @@ use GGE::Perl6Regex;
 
 class GGE::OPTable {
     # RAKUDO: Must define these within the class for them to be visible.
-    constant GGE_OPTABLE_EXPECT_TERM = 0x01;
-    constant GGE_OPTABLE_EXPECT_OPER = 0x02;
+    constant GGE_OPTABLE_EXPECT_TERM   = 0x01;
+    constant GGE_OPTABLE_EXPECT_OPER   = 0x02;
+
+    constant GGE_OPTABLE_TERM          = 0x10;
+    constant GGE_OPTABLE_POSTFIX       = 0x20;
+    constant GGE_OPTABLE_CLOSE         = 0x30;
+    constant GGE_OPTABLE_POSTCIRCUMFIX = 0x80;
+    constant GGE_OPTABLE_CIRCUMFIX     = 0x90;
 
     has %!tokens;
     has %!keys;
     has %!klen;
 
     has %!sctable =
-            'term:'          => { expect => 0x0201             },
-            'postfix:'       => { expect => 0x0202, arity => 1 },
+            'term:'          => { syncat => GGE_OPTABLE_TERM,
+                                  expect => 0x0201             },
+            'postfix:'       => { syncat => GGE_OPTABLE_POSTFIX,
+                                  expect => 0x0202, arity => 1 },
+            'close:'         => { syncat => GGE_OPTABLE_CLOSE,
+                                  expect => 0x0202             },
             'prefix:'        => { expect => 0x0101, arity => 1 },
             'infix:'         => { expect => 0x0102, arity => 2 },
+            'circumfix:'     => { syncat => GGE_OPTABLE_CIRCUMFIX,
+                                  expect => 0x0101, arity => 1 },
     ;
 
     method newtok($name, *%opts) {
@@ -35,11 +47,18 @@ class GGE::OPTable {
         elsif %opts<tighter> -> $t {
             %opts<precedence> = %!tokens{$t}<precedence> ~ '>';
         }
+        my $key = $name.substr($name.index(':') + 1);
         %opts<assoc> //= 'left';
         %opts<name> = $name;
         %!tokens{$name} = %opts;
+        if defined (my $ix = $key.index(' ')) {
+            my $keyclose = $key.substr($ix + 1);
+            $key .= substr(0, $ix);
+            self.newtok("close:$keyclose", :equiv($name),
+                        :expect(%opts<expectclose> // 0x0202),
+                        :nows(%opts<nows>));
+        }
 
-        my $key = $name.substr($name.index(':') + 1);
         my $keylen = $key.chars;
         my $key_firstchar = $key.substr(0, 1);
         # RAKUDO: max=
@@ -57,22 +76,25 @@ class GGE::OPTable {
         my $pos = 0;
         my (@termstack, @tokenstack, @operstack);
         my $expect = GGE_OPTABLE_EXPECT_TERM;
-        my &shift_oper = -> $key {
-            my $name = $key.substr($key.index(':') + 1);
+        my &shift_oper = -> $name, $key {
             my $op = GGE::Match.new(:from($pos),
-                                    :to($pos + $name.chars),
+                                    :to($pos + $key.chars),
                                     :target($text));
-            $op<type> = $key;
-            push @tokenstack, $op;
+            $op<type> = $name;
+            push @tokenstack, %!tokens{$name};
             push @operstack, $op;
             $pos = $op.to;
-            $expect = %!tokens{$key}<expect> +> 8;
+            $expect = %!tokens{$name}<expect> +> 8;
         };
         my &reduce = {
-            pop @tokenstack;
+            my $top = pop @tokenstack;
             my $oper = pop @operstack;
+            if $top<syncat> == GGE_OPTABLE_CLOSE {
+                $top = pop @tokenstack;
+                $oper = pop @operstack;
+            }
             my @temp;
-            my $arity = %!tokens{$oper<type>}<arity>;
+            my $arity = $top<arity>;
             for ^$arity {
                 @temp.push(pop(@termstack));
             }
@@ -80,8 +102,7 @@ class GGE::OPTable {
                 for reverse ^$arity {
                     $oper.push( @temp[$_] );
                 }
-                if %!tokens{$oper<type>}<assoc> eq 'list'
-                   && $oper<type> eq @temp[1]<type> {
+                if $top<assoc> eq 'list' && $oper<type> eq @temp[1]<type> {
 
                     @temp[1].push($oper.llist[1]);
                     $oper = @temp[1];
@@ -105,6 +126,11 @@ class GGE::OPTable {
             my $key = $text.substr($pos, $maxlength);
             my $found_oper = False;
             loop {
+                if $text.substr($pos, $key.chars) ne $key {
+                    last if $key eq '';
+                    $key .= chop();
+                    next;
+                }
                 for (%!keys{$key} // []).list -> $token {
                     my $name = $token<name>;
                     if $token.exists('parsed') {
@@ -129,20 +155,31 @@ class GGE::OPTable {
                     }
                     elsif $expect +& $token<expect> {
                         if @operstack {
-                            my $top = @operstack[*-1];
-                            my $toptype = $top<type>;
-                            # XXX: You guessed it -- the addition of a hundred
-                            #      equals signs is kind of a hack.
-                            my $topprec = %!tokens{$toptype}<precedence>
-                                          ~ '=' x 100;
-                            my $prec = %!tokens{$name}<precedence> ~ '=' x 100;
-                            my $topassoc = %!tokens{$toptype}<assoc>;
-                            if $topprec gt $prec
-                               || $topprec eq $prec && $topassoc ne 'right' {
-                                reduce;
+                            my $top = @tokenstack[*-1];
+                            my $topcat = $top<syncat>;
+                            if $token<syncat> == GGE_OPTABLE_CLOSE {
+                                if $topcat < GGE_OPTABLE_CIRCUMFIX {
+                                    reduce;
+                                }
+                            }
+                            elsif $topcat >= GGE_OPTABLE_POSTCIRCUMFIX {
+                                # go directly to shift
+                            }
+                            else {
+                                # XXX: You guessed it -- the addition of
+                                #      a hundred equals signs is kind of
+                                #      a hack.
+                                my $topprec = $top<precedence> ~ '=' x 100;
+                                my $prec = $token<precedence> ~ '=' x 100;
+                                my $topassoc = $top<assoc>;
+                                if $topprec gt $prec
+                                   || $topprec eq $prec
+                                      && $topassoc ne 'right' {
+                                    reduce;
+                                }
                             }
                         }
-                        shift_oper($name);
+                        shift_oper($name, $key);
                         $found_oper = True;
                         last;
                     }
