7a9fac6469210a78bc4958166e1b0056bd9497b0
Carl Masak <cmasak@gmail.com>
2009-10-10 03:05:08 +0200

    [GGE::OPTable] now handles list associativity

--- lib/GGE/OPTable.pm
+++ lib/GGE/OPTable.pm
@@ -29,12 +29,30 @@ class GGE::OPTable {
         my $pos = 0;
         my (@termstack, @tokenstack, @operstack);
         my $expect = GGE_OPTABLE_EXPECT_TERM;
+        my &shift_oper = -> $key {
+            my $name = $key.substr(6);
+            my $op = GGE::Match.new(:from($pos),
+                                    :to($pos + $name.chars),
+                                    :target($text));
+            $op<type> = $key;
+            push @tokenstack, $op;
+            push @operstack, $op;
+            $pos = $op.to;
+            $expect = GGE_OPTABLE_EXPECT_TERM;
+        };
         my &reduce = {
             pop @tokenstack;
             my $oper = pop @operstack;
             my @temp = pop(@termstack), pop(@termstack);
             $oper.push( @temp[1] );
             $oper.push( @temp[0] );
+            say [$oper<type>, @temp[1]<type>].perl;
+            if %!tokens{$oper<type>}<assoc> eq 'list'
+               && $oper<type> eq @temp[1]<type> {
+
+                @temp[1].push($oper.llist[1]);
+                $oper = @temp[1];
+            }
             push @termstack, $oper;
         };
         while $pos < $text.chars {
@@ -57,22 +75,18 @@ class GGE::OPTable {
                         if @operstack {
                             my $top = @operstack[*-1];
                             my $toptype = $top<type>;
-                            my $topprec = %!tokens{$toptype}<precedence>;
-                            my $prec = %!tokens{$key}<precedence>;
+                            # XXX: You guessed it -- the addition of a hundred
+                            #      equals signs is kind of a hack.
+                            my $topprec = %!tokens{$toptype}<precedence>
+                                          ~ '=' x 100;
+                            my $prec = %!tokens{$key}<precedence> ~ '=' x 100;
                             my $topassoc = %!tokens{$toptype}<assoc>;
                             if $topprec gt $prec
-                               || $topprec eq $prec && $topassoc eq 'left' {
+                               || $topprec eq $prec && $topassoc ne 'right' {
                                 reduce;
                             }
                         }
-                        my $op = GGE::Match.new(:from($pos),
-                                                :to($pos + $name.chars),
-                                                :target($text));
-                        $op<type> = $key;
-                        push @tokenstack, $op;
-                        push @operstack, $op;
-                        $pos = $op.to;
-                        $expect = GGE_OPTABLE_EXPECT_TERM;
+                        shift_oper($key);
                         last;
                     }
                 }
