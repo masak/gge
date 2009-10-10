2b81590adfd6db52b068c8575cb3c30a0c6291fe
Carl Masak <cmasak@gmail.com>
2009-10-10 06:05:33 +0200

    [GGE::OPTable] found/fixed bug
    Seems the longest lengths were overwritten by shorter ones, making postfix:--
    not match since a postfix:- was defined after it.

--- lib/GGE/OPTable.pm
+++ lib/GGE/OPTable.pm
@@ -42,7 +42,8 @@ class GGE::OPTable {
         my $keylen = $key.chars;
         my $key_firstchar = $key.substr(0, 1);
         # RAKUDO: max=
-        if %!klen{$key_firstchar} // -Inf < $keylen {
+        if $key_firstchar && !%!klen.exists($key_firstchar)
+           || %!klen{$key_firstchar} < $keylen {
             %!klen{$key_firstchar} = $keylen;
         }
 
